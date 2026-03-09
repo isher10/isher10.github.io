003e246fb37db59234bc188a05713a1259f59ef5
Isher education <ishwarbeniwal770@gmail.com>
2026-03-09 11:53:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ reader.onload = function(e){
 
 previewImage.src = e.target.result;
 previewImage.style.display = "block";
-
+document.getElementById("beforeImage").src = e.target.result;
 }
 
 reader.readAsDataURL(file);
